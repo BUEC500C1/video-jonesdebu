f21f5024fd115561ea83fc3ae21de40cb4164476
jonesdebu <33497013+jonesdebu@users.noreply.github.com>
2020-02-12 18:18:02 -0500

    implementing queue
    functions not running on queue.get()

---
+++ my_queue.py
@@ -0,0 +1,45 @@
+from __future__ import annotations
+import _thread
+from time import sleep
+import queue
+import subprocess
+
+
+
+# create queue with constructor
+q = queue.Queue(0)
+
+#define some dummy worker functions for testing
+def worker1( b ):
+    return "worker1"
+
+def worker2( b ):
+    return "worker2"
+
+def worker3( b ):
+    return "worker3"
+
+# get function and arguments from the queue to be run
+def dest_thread( a ):
+    func, args = q.get()
+    func(*args)
+# start a new thread to put functions
+#dest_thread("a")
+print("start")
+#sleep( 1 )
+q.put( (worker1, "b") )
+#sleep( 1 )
+q.put( (worker2, "b") )
+#sleep( 1 )
+q.put( (worker3, "b") )
+
+func, args = q.get()
+func(*args)
+
+print("stop")
+
+if q.empty() is True:
+    print("queue is empty")
+else:
+    print("queue is not empty")
+    print(q.qsize())
--- twitter_conversion_api.py
+++ twitter_conversion_api.py
@@ -1,1 +1,7 @@
 import tweepy
+
+## TODO:
+#   1.  get twitter feed using old twitter api from last homework assignment
+#   2.  convert text from feed to image
+#   3. display image?
+#   4

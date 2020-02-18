1a3b94f16f2e7f73bb495c1bf33d93ced7b53a8a
jonesdebu <33497013+jonesdebu@users.noreply.github.com>
2020-02-17 22:21:35 -0500

    Update myqueue.oy
    working queue and threads

--- my_queue.py
+++ my_queue.py
@@ -1,40 +1,48 @@
-from __future__ import annotations
-import _thread
+#from __future__ import annotations
 from time import sleep
 import queue
 import subprocess
+import threading
 
-
+#need to import the twitter api function and pass the arguments as arguments to this queue starter function
 
 # create queue with constructor
-q = queue.Queue(0)
+q = queue.Queue()
 
 #define some dummy worker functions for testing
-def worker1( b ):
-    return "worker1"
+def worker1( a ):
+    print("worker1")
 
-def worker2( b ):
-    return "worker2"
+def worker2( a ):
+    print("worker2")
 
-def worker3( b ):
-    return "worker3"
+def worker3( a ):
+    print("worker3")
 
 # get function and arguments from the queue to be run
-def dest_thread( a ):
-    func, args = q.get()
-    func(*args)
+#def dest_thread( a ):
+    #func, args = q.get()
+    #func(*args)
+
 # start a new thread to put functions
-#dest_thread("a")
+
 print("start")
 #sleep( 1 )
-q.put( (worker1, "b") )
+q.put( (worker1, 'a') )
 #sleep( 1 )
-q.put( (worker2, "b") )
+q.put( (worker2, 'a') )
 #sleep( 1 )
-q.put( (worker3, "b") )
+q.put( (worker3, 'a') )
 
-func, args = q.get()
-func(*args)
+index = 1;
+threads = []
+while q.empty() is False:
+    items = q.get()
+    func = items[0]
+    args = items[1:]
+    thread = threading.Thread(target=func, args=args)
+    thread.start()
+    threads.append(thread)
 
 print("stop")
 
@@ -44,4 +52,10 @@ else:
     print("queue is not empty")
     print(q.qsize())
 
+#return statement does not work to execute in thread
+#worker2() # prints nothing
+#a = worker2() # printing a prints what worker2() returns
+#print(a)
+
 #TODO: 1. Figure out how to get the functions to run aftter getting them from Queue.get()
+# start thread then subprocess library to run thread

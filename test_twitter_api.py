c00292441e469576369eabf8bd14ecffeea3ac77
jonesdebu <33497013+jonesdebu@users.noreply.github.com>
2020-03-01 23:43:13 -0500

    Update test_twitter_api.py

--- test_twitter_api.py
+++ test_twitter_api.py
@@ -4,4 +4,4 @@ from twitter_api import create_api, user_images, vid_creator
 def test_user_images():
     api = create_api("keys")
     images = user_images(api, 'Donovan01060515')
-    assert len[images] == 2
+    assert len(images) == 2
